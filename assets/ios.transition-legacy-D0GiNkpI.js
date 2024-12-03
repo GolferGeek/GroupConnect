af7d4518754195dc452802971a810145778b5761
golfergeek <golfergeet@gmail.com>
2024-12-03 15:17:14 -0600

    Updates

--- assets/ios.transition-legacy-D0GiNkpI.js
+++ assets/ios.transition-legacy-D0GiNkpI.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Ayepr4E6.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-DOZ4qsaX.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
